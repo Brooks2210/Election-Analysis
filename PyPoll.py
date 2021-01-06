d8836976622860fbc995b8792077e591776c033e
Brooks2210 <74885617+Brooks2210@users.noreply.github.com>
2021-01-05 20:38:19 -0700

    Add files via upload

---
+++ PyPoll.py
@@ -0,0 +1,5 @@
+# The data we need to retrieve.
+# The total number of votes cast.
+# A complete list of candidates who recieved votes.
+# The total number of votes each candidate won.
+# the Winner of the election based on popular vote.
